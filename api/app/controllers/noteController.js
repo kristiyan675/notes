f25d572987814e3c5b0a5cc2af19eb16868fe44f
kristiyan675 <74766712+kristiyan675@users.noreply.github.com>
2024-06-02 12:43:12 +0300

    init

---
+++ api/app/controllers/noteController.js
@@ -0,0 +1,65 @@
+const pool = require("../../db");
+
+// Get all notes
+exports.getAllNotes = (req, res) => {
+  console.log("here");
+  pool.query("SELECT * FROM notes", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+};
+
+exports.getNoteById = (req, res) => {
+  const id = parseInt(req.params.id);
+  pool.query("SELECT * FROM notes WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows[0]);
+  });
+};
+
+exports.createNote = (req, res) => {
+  const { title, content } = req.body;
+  pool.query(
+    "INSERT INTO notes (title, content) VALUES ($1, $2) RETURNING *",
+    [title, content],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(201).json(results.rows[0]);
+    }
+  );
+};
+
+exports.updateNote = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, content } = req.body;
+  pool.query(
+    "UPDATE notes SET title = $1, content = $2, updated_at = CURRENT_TIMESTAMP WHERE id = $3 RETURNING *",
+    [title, content, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).json(results.rows[0]);
+    }
+  );
+};
+
+exports.deleteNote = (req, res) => {
+  const id = parseInt(req.params.id);
+  pool.query(
+    "DELETE FROM notes WHERE id = $1 RETURNING *",
+    [id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).json(results.rows[0]);
+    }
+  );
+};
---
+++ api/app/index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const noteRoutes = require("./routes/noteRoutes");
+const app = express();
+app.use(express.json());
+app.use("/notes", noteRoutes);
+module.exports = app;
