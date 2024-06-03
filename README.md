800455027d849c0a7d3396ed00c952def563fb2b
kristiyan675 <vasilev.kristiyan.v@gmail.com>
2024-06-03 11:51:13 +0300

    crud feedback and readme

---
+++ README.md
@@ -0,0 +1,92 @@
+# Notes App
+
+This project is a full-stack application for taking notes, built with a React frontend using Ant Design for styling, and a Node.js backend with Express and PostgreSQL for handling CRUD operations.
+
+## Features
+
+- Create, read, update, and delete notes.
+- Responsive design with Ant Design components.
+- Pagination for displaying notes.
+- Integration with PostgreSQL for data persistence.
+
+## Technologies Used
+
+### Backend
+
+- Node.js v18.12.1
+- Express v4.19.2
+- PostgreSQL v16.3
+- pg v8.11.5
+
+### Frontend
+
+- React v18.2.0
+- Ant Design v5.17.4
+- React Router DOM v6.23.1
+- React Paginate v8.2.0
+
+## Getting Started
+
+Make sure you have NodeJs installed.
+
+Install and setup local PostgresSQL server.
+
+### Backend Setup
+
+From terminal:
+
+1. Clone the repository:
+   ```sh
+   git clone https://github.com/kristiyan675/notes.git
+   ```
+2. Update the init.sh and db.js files with your personal local database credentials.
+
+3. Navigate to api folder
+
+   ```sh
+   cd ./api
+
+   ```
+
+4. Install dependencies
+
+   ```sh
+   npm run install
+
+   ```
+
+5. Make sure you have nothin running port 3000.
+   Run init.sh file (If you're on windows you can use git bash for running the script).
+   This will connect and create a new database and table.
+   ```sh
+   ./init.sh
+   ```
+
+### Frontend Setup
+
+Once the server is running and the database has been created successfully.
+
+From terminal:
+
+1. Navigate to api folder
+
+   ```sh
+   cd ./app
+
+   ```
+
+2. Install dependencies
+
+   ```sh
+   npm run install
+
+   ```
+
+3. Run the application
+
+   ```sh
+   npm run dev
+
+   ```
+
+4. Open app in the browser
--- app/src/context/NoteContext.jsx
+++ app/src/context/NoteContext.jsx
@@ -1,9 +1,18 @@
 import { createContext, useState, useContext, useEffect } from "react";
+import { notification } from "antd";
 
 const NotesContext = createContext();
+const apiUrl = "http://localhost:3000";
 
 export const useNotes = () => {
   return useContext(NotesContext);
+};
+
+const openNotification = (type, message, description) => {
+  notification[type]({
+    message,
+    description,
+  });
 };
 
 export const NotesProvider = ({ children }) => {
@@ -12,11 +21,12 @@ export const NotesProvider = ({ children }) => {
   useEffect(() => {
     const fetchNotes = async () => {
       try {
-        const response = await fetch("http://localhost:3000/notes");
+        const response = await fetch(`${apiUrl}/notes`);
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const data = await response.json();
+        console.log("here");
         setNotes(data);
       } catch (err) {
         console.log(err);
@@ -28,7 +38,7 @@ export const NotesProvider = ({ children }) => {
 
   const getNoteById = async (id) => {
     try {
-      const response = await fetch(`http://localhost:3000/notes/${id}`, {
+      const response = await fetch(`${apiUrl}/notes/${id}`, {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
@@ -47,7 +57,7 @@ export const NotesProvider = ({ children }) => {
 
   const createNote = async (note) => {
     try {
-      const response = await fetch("http://localhost:3000/notes", {
+      const response = await fetch(`${apiUrl}/notes`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -60,6 +70,12 @@ export const NotesProvider = ({ children }) => {
       }
 
       const data = await response.json();
+
+      openNotification(
+        "success",
+        "Note Creation",
+        "Note has been successfully created."
+      );
       setNotes((prevNotes) => [...prevNotes, data]);
     } catch (err) {
       console.log(err);
@@ -68,7 +84,7 @@ export const NotesProvider = ({ children }) => {
 
   const updateNote = async (updatedNote, id) => {
     try {
-      const response = await fetch(`http://localhost:3000/notes/${id}`, {
+      const response = await fetch(`${apiUrl}/notes/${id}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
@@ -81,6 +97,11 @@ export const NotesProvider = ({ children }) => {
       }
 
       const data = await response.json();
+      openNotification(
+        "success",
+        "Note Update",
+        "Note has been successfully updated."
+      );
       setNotes((prevNotes) =>
         prevNotes.map((note) => (note.id === id ? data : note))
       );
@@ -92,14 +113,18 @@ export const NotesProvider = ({ children }) => {
 
   const deleteNote = async (id) => {
     try {
-      const response = await fetch(`http://localhost:3000/notes/${id}`, {
+      const response = await fetch(`${apiUrl}/notes/${id}`, {
         method: "DELETE",
       });
 
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
-
+      openNotification(
+        "success",
+        "Note Update",
+        "Note has been successfully deleted."
+      );
       setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
     } catch (err) {
       console.log(err);
